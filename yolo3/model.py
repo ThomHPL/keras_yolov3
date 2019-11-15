6c1923d2166569a9c9d20250bba820d07d881fcf
ThomHPL <thomas.herpoel@gmail.com>
2019-11-15 14:46:30 +0100

    change yolo3

--- yolo3/model.py
+++ yolo3/model.py
@@ -5,9 +5,8 @@ from functools import wraps
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import backend as K
-from tensorflow.keras.layers import LeakyReLU, Conv2D, Add, ZeroPadding2D, UpSampling2D, Concatenate, MaxPooling2D
-from tensorflow.keras.layers.normalization import BatchNormalization
-from tensorflow.keras.models import Model
+from tensorflow.keras.layers import BatchNormalization, LeakyReLU, Conv2D, Add, ZeroPadding2D, UpSampling2D, Concatenate, MaxPooling2D
+from tensorflow.keras import Model
 from tensorflow.keras.regularizers import l2
 
 from yolo3.utils import compose
